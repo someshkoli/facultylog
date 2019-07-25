68f572b16286f475681e434c8c124cfa6e99c833
Somesh <kolisomesh27@gmail.com>
2019-07-25 16:45:52 +0530

    semi final push

--- app/Http/Controllers/faculty.php
+++ app/Http/Controllers/faculty.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use DB;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -8,37 +9,58 @@ use Illuminate\Http\Response;
 
 class faculty extends Controller
 {
-    public function getdepartment(Request $request){
-       $data=DB::connection('RAIT')->table('Department')->get()->toJson();
-       return response($data);
+    public function getdepartment(Request $request)
+    {
+        $data = DB::connection('RAIT')->table('Department')->get()->toJson();
+        return response($data);
     }
 
-    public function get_faculty(Request $request,Response $response){
-        //echo (string)$request->all();
-        $courses=DB::connection('RAIT')->table('course')->select('Subject_name')->where('Year',"=",$request->all()['params']['year'])->get();
-        $course=array();
-        $faculty=array();
-        $faculties=DB::connection('RAIT')->table('faculty')->select(DB::raw('concat(First_name," ",Middle_name," ",Last_name) AS name'))->get();
-        foreach($courses as $c){
-            array_push($course,$c->Subject_name);
+    public function get_all_faculty(Request $request)
+    {
+        $courses = DB::connection('RAIT')->table('course')->select('Subject_name')->get();
+        $course = array();
+        $faculty = array();
+        $faculties = DB::connection('RAIT')->table('faculty')->select('sdrn', DB::raw('concat(First_name," ",Middle_name," ",Last_name) AS name'))->get();
+        foreach ($courses as $c) {
+            array_push($course, $c->Subject_name);
         }
 
-        foreach($faculties as $f){
-            if($f->name==null){
+        foreach ($faculties as $f) {
+            if ($f->name == null) {
                 continue;
-            }
-            else{
-            array_push($faculty,$f->name);
-
+            } else {
+                array_push($faculty, $f);
             }
         }
 
         return json_encode([
-            'course'=> $course,
+            'course' => $course,
             'faculty' => $faculty
         ]);
+    }
 
+    public function get_faculty(Request $request, Response $response)
+    {
+        //echo (string)$request->all();
+        $courses = DB::connection('RAIT')->table('course')->select('Subject_name')->where('Year', "=", $request->all()['params']['year'])->get();
+        $course = array();
+        $faculty = array();
+        $faculties = DB::connection('RAIT')->table('faculty')->select('sdrn', DB::raw('concat(First_name," ",Middle_name," ",Last_name) AS name'))->get();
+        foreach ($courses as $c) {
+            array_push($course, $c->Subject_name);
+        }
+
+        foreach ($faculties as $f) {
+            if ($f->name == null) {
+                continue;
+            } else {
+                array_push($faculty, $f);
+            }
+        }
+
+        return json_encode([
+            'course' => $course,
+            'faculty' => $faculty
+        ]);
     }
 }
-
-
--- app/Http/Controllers/time_table_controller.php
+++ app/Http/Controllers/time_table_controller.php
@@ -17,10 +17,25 @@ class time_table_controller extends Controller
         $time_table = new time_table($request->all()['college']);
         // echo $request;
         $keys = array_keys((array) $request->all()['params']);
-        $query = $time_table->get();
+        $query = DB::connection($request->all()['college'])->table('time_table')->get();
+        $data = array();
+        $faculty = DB::connection('RAIT')->table('faculty')
+            ->select('sdrn', DB::raw('concat(First_name," ",Middle_name," ",Last_name) AS name'))
+            ->get();
 
         foreach ($keys as $key) {
+            if ($request->all()['params'][$key] == "") {
+                continue;
+            }
             $query = $query->where($key, $request->all()['params'][$key]);
+        }
+
+        foreach ($query as $q) {
+            foreach ($faculty as $f) {
+                if ($f->sdrn == $q->sdrn) {
+                    $q->sdrn = $f->name;
+                }
+            }
         }
         return response($query);
     }
@@ -37,108 +52,92 @@ class time_table_controller extends Controller
     //give faculty curent time to principal
     public function faculty_current_time(Request $request, Response $response)
     {
-        $time_table = new time_table();
+        $time_table = new time_table($request->all()['college']);
         $data = $time_table->all()->where(function ($query) {
             $query->where('sdrn', $request->all()['sdrn']);
         });
     }
 
-    public function class_time_table_view(Request $request, Response $response)
-    {
-        $time_table = new time_table();
-    }
+    // public function class_time_table_view(Request $request, Response $response)
+    // {
+    //     $time_table = new time_table();
+    // }
 
     public function test(Request $request, Response $response)
     {
-        $days = ["MON", "TUE", "WED", "THUR", "FRI", "SAT"];
+        $days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         $final_time_table = array();
-        // foreach($days as $day1){
-        //     echo $day1;
-        //     $day=array();
-        //     $data = DB::connection("RAIT")->table("time_table", "faculty")
-
-        //     ->select(
-        //         'time_table.subject',
-        //         'time_table.day',
-        //         'time_table.start_time',
-        //         'time_table.end_time',
-        //         'time_table.sdrn',
-        //         'time_table.room',
-        //         'time_table.batch',
-        //         DB::raw('concat(faculty.First_name," ",faculty.Middle_name," ",faculty.Last_name) AS name')
-        //     )
-        //     ->join('faculty', 'time_table.sdrn', '=', 'faculty.sdrn')
-        //     ->where("day", "=",$day1)
-        //     ->get();
-
-        //     foreach ($data as $d) {
-        //         $temp = [
-        //             'day' => $d->day,
-        //             $d->start_time . "-" . $d->end_time => [
-        //                 'subject' => $d->subject,
-        //                 'class' => $d->room,
-        //                 'faculty' => $d->name
-        //             ]
-        //         ];
-        //         array_push($day, $temp);
-        //     }
-
-        //     array_push($final_time_table,[$day1=>$day]);
-        // }
-
-        $day = array();
-        $data = DB::connection("RAIT")->table("time_table", "faculty")
-
-            ->select(
-                'time_table.subject',
-                'time_table.day',
-                'time_table.start_time',
-                'time_table.end_time',
-                'time_table.sdrn',
-                'time_table.room',
-                'time_table.batch',
-                DB::raw('concat(faculty.First_name," ",faculty.Middle_name," ",faculty.Last_name) AS name')
-            )
-            ->join('faculty', 'time_table.sdrn', '=', 'faculty.sdrn')
-            // ->where("day", "=", $day1)
-            ->get();
-
-        foreach ($data as $d) {
-            $temp = [
-                'day' => $d->day,
-                $d->start_time . "-" . $d->end_time => [
-                    'subject' => $d->subject,
-                    'class' => $d->room,
-                    'faculty' => $d->name
-                ]
+        foreach ($days as $day) {
+            $dayd = [
+                'day' => $day
             ];
-            array_push($day, $temp);
+            $data = DB::connection($request->all()['college'])->table("time_table", "faculty")
+                ->select(
+                    'time_table.subject',
+                    'time_table.day',
+                    'time_table.start_time',
+                    'time_table.end_time',
+                    'time_table.sdrn',
+                    'time_table.room',
+                    'time_table.batch',
+                    DB::raw('concat(faculty.First_name," ",faculty.Last_name) AS name')
+                )
+                ->join('faculty', 'time_table.sdrn', "=", 'faculty.sdrn')
+                ->where('division', "=", $request->all()['params']['division'])
+                ->where('day', $day)
+                ->get();
+            //echo (string)$day;
+            // response($data);
+            foreach ($data as $d) {
+                // echo "213131";
+                // return json_encode($data);
+                $temp = [
+                    $d->start_time . "-" . $d->end_time => [
+                        'subject' => $d->subject,
+                        'class' => $d->room,
+                        'faculty' => $d->name
+                    ]
+                ];
+                $dayd = array_merge($dayd, $temp);
+                //return json_encode($dayd);
+            }
+
+            array_push($final_time_table, $dayd);
         }
-        return response($day);
+        return response($final_time_table);
     }
 
     //add a new schedule record
     public function store(Request $request)
     {
-        $sub_short = DB::connection($request->all()['college'])->table('course')->select('Subject_shortname')->where('Subject_name', '=', $request->all()['params']['subject'])->get();
+        $sub_short = DB::connection($request->all()['college'])
+            ->table('course')
+            ->select('Subject_shortname')
+            ->where('Subject_name', '=', $request->all()['params']['subject'])
+            ->get();
+        $faculty_sdrn = DB::connection($request->all()['college'])
+            ->table('faculty')
+            ->select('Sdrn')
+            ->where('Sdrn', '=', $request->all()['params']['sdrn'])
+            ->get();
         $keys = array_keys((array) $request->all()['params']);
         $time_table = new time_table($request->all()['college']);
         $time_table->department = $request->all()['params']['department'];
         $time_table->start_time = $request->all()['params']['start_time'];
         $time_table->room = $request->all()['params']['room'];
         $time_table->division = $request->all()['params']['division'];
-        $time_table->sdrn = $request->all()['params']['sdrn'];
+        $time_table->sdrn = $faculty_sdrn[0]->Sdrn;
         $time_table->end_time = $request->all()['params']['end_time'];
         $time_table->batch = $request->all()['params']['batch'];
         $time_table->subject = $sub_short[0]->Subject_shortname;
         $time_table->day = $request->all()['params']['day'];
         $time_table->year = $request->all()['params']['year'];
         $time_table->save();
-        return Response("hello");
+        return Response($faculty_sdrn);
     }
 
-    public function destroy($id)
+    public function destroy(Request $request, Response $response)
     {
-        //
+        DB::connection($request->all()['college'])->table('time_table')->where('srno', $request->all()['params']['srno'])->delete();
     }
 }
